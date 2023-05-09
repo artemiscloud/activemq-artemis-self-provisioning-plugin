5a52ef95019eabd268847ad1bf458ff91f6b5e61
Msarawan <msarawan@redhat.com>
2023-05-10 00:12:00 +0530

    Modified broker form component

--- src/brokers/add-broker/components/AddBrokerForm/FormPage.component.tsx
+++ src/brokers/add-broker/components/AddBrokerForm/FormPage.component.tsx
@@ -1,38 +1,81 @@
 import { FC, useState } from 'react';
-import { Form, FormGroup, TextInput, Alert } from '@patternfly/react-core';
+import {
+  Form,
+  FormGroup,
+  TextInput,
+  Alert,
+  Button,
+  AlertGroup,
+  Grid,
+  GridItem,
+  ActionListItem,
+  ActionList,
+} from '@patternfly/react-core';
 import { useTranslation } from '../../../../i18n';
+import { Link } from 'react-router-dom';
+type validate = 'success' | 'warning' | 'error' | 'default';
 
 export const FormPage: FC = () => {
   const { t } = useTranslation();
   const [name, setName] = useState('');
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+  const [validated, setValidated] = useState<validate>('error');
 
   const handleNameChange = (name: string) => {
     setName(name);
+    if (name === ' ') {
+      setValidated('default');
+    } else if (!isNaN(+name)) {
+      setValidated('success');
+    } else {
+      setValidated('error');
+    }
+  };
+
+  const onSubmitHandler = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (name) {
+      setShowAlert(true);
+    }
   };
 
   return (
-    <Form>
-      <Alert
-        variant="info"
-        isInline
-        title={t('small_note')}
-        className="pf-u-mx-sm"
-      />
-      <FormGroup
-        label={t('name')}
-        isRequired
-        fieldId="name"
-        className="pf-u-mx-md"
-      >
-        <TextInput
-          isRequired
-          type="text"
-          id="name"
-          name="name"
-          value={name}
-          onChange={handleNameChange}
-        />
-      </FormGroup>
-    </Form>
+    <Grid className="pf-u-mx-md">
+      <GridItem span={6}>
+        {showAlert && <Alert variant="info" isInline title={t('info_alert')} />}
+        <Form
+          onSubmit={(e: React.FormEvent<HTMLFormElement>): void => {
+            e.preventDefault();
+          }}
+        >
+          <FormGroup
+            label={t('name')}
+            isRequired
+            fieldId="name"
+            validated={validated}
+          >
+            <TextInput
+              isRequired
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </FormGroup>
+          <ActionList>
+            <ActionListItem>
+              <Button variant="primary" type="submit" onClick={onSubmitHandler}>
+                Submit
+              </Button>
+              <AlertGroup>{showAlert}</AlertGroup>
+            </ActionListItem>
+            <Button variant="link">
+              <Link to="/k8s/all-namespaces/brokers">Cancel</Link>
+            </Button>
+          </ActionList>
+        </Form>
+      </GridItem>
+    </Grid>
   );
 };

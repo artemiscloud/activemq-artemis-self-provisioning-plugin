e6cadb46905373750f193f894dca9d31350d78ce
Msarawan <msarawan@redhat.com>
2023-06-02 17:58:27 +0530

    Added support for refresh off and updated the polling time interval dropdown on top of charts

--- src/brokers/metrics/components/CardBrokerCPUUsageMetrics/CardBrokerCPUUsageMetrics.tsx
+++ src/brokers/metrics/components/CardBrokerCPUUsageMetrics/CardBrokerCPUUsageMetrics.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Card, CardBody } from '@patternfly/react-core';
 import {
   ChartCPUUsage,
@@ -8,6 +8,10 @@ import { useTranslation } from '../../../../i18n';
 import { CardBodyLoading } from '../CardBodyLoading/CardBodyLoading';
 import { EmptyStateNoMetricsData } from '../EmptyStateNoMetricsData/EmptyStateNoMetricsData';
 import { ChartTitle } from '../ChartTitle';
+import {
+  DropdownWithToggle,
+  IDropdownOption,
+} from '../../../../shared-components/DropdownWithToggle';
 
 export type CardBrokerCPUUsageMetricsProps = ChartCPUUsageProps & {
   isInitialLoading: boolean;
@@ -25,6 +29,92 @@ export const CardBrokerCPUUsageMetrics: FC<CardBrokerCPUUsageMetricsProps> = ({
   formatSeriesTitle,
 }) => {
   const { t } = useTranslation();
+  const [selectedValue, setSelectedValue] = useState<string>('0');
+
+  const dropdownItems: IDropdownOption[] = [
+    {
+      key: 'Refresh Off',
+      value: '0',
+      label: t('refresh_off'),
+      isDisabled: false,
+    },
+    {
+      key: '15s',
+      value: '15s',
+      label: t('15_seconds'),
+      isDisabled: false,
+    },
+    {
+      key: '30s',
+      value: '30s',
+      label: t('30_seconds'),
+      isDisabled: false,
+    },
+    {
+      key: '1m',
+      value: '1m',
+      label: t('1_minute'),
+      isDisabled: false,
+    },
+    {
+      key: '5m',
+      value: '5m',
+      label: t('5_minutes'),
+      isDisabled: false,
+    },
+    {
+      key: '15m',
+      value: '15m',
+      label: t('15_minutes'),
+      isDisabled: false,
+    },
+    {
+      key: '30m',
+      value: '30m',
+      label: t('30_minutes'),
+      isDisabled: false,
+    },
+    {
+      key: '1h',
+      value: '1h',
+      label: t('1_hour'),
+      isDisabled: false,
+    },
+    {
+      key: '6h',
+      value: '6h',
+      label: t('6_hours'),
+      isDisabled: false,
+    },
+    {
+      key: '1d',
+      value: '1d',
+      label: t('1_day'),
+      isDisabled: false,
+    },
+    {
+      key: '2d',
+      value: '2d',
+      label: t('2_days'),
+      isDisabled: false,
+    },
+    {
+      key: '1w',
+      value: '1w',
+      label: t('1_week'),
+      isDisabled: false,
+    },
+    {
+      key: '2w',
+      value: '2w',
+      label: t('2_weeks'),
+      isDisabled: false,
+    },
+  ];
+
+  const onSelectOption = (value: string) => {
+    setSelectedValue(value);
+  };
 
   return (
     <Card data-test-id={'metrics-broker-cpu-usage'}>
@@ -41,6 +131,16 @@ export const CardBrokerCPUUsageMetrics: FC<CardBrokerCPUUsageMetricsProps> = ({
           default:
             return (
               <>
+                <div className="pf-u-ml-sm pf-u-mt-sm pf-u-mb-sm">
+                  <DropdownWithToggle
+                    id="polling-dropdown"
+                    toggleId="polling-dropdowntoggle"
+                    items={dropdownItems}
+                    value={selectedValue}
+                    onSelectOption={onSelectOption}
+                    isLabelAndValueNotSame={true}
+                  />
+                </div>
                 <ChartTitle
                   title={t('cpu_usage')}
                   helperText={t('cpu_usage_help_text')}

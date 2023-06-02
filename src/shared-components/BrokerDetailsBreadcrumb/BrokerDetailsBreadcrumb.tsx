4171dc35aa54aea34c993ef20816d1d3617c3a4c
Ajay Pratap <ajaypratap003@users.noreply.github.com>
2023-06-03 03:48:07 +0530

    add kebab options on details page (#49)
    for edit and delete broker

--- src/shared-components/BrokerDetailsBreadcrumb/BrokerDetailsBreadcrumb.tsx
+++ src/shared-components/BrokerDetailsBreadcrumb/BrokerDetailsBreadcrumb.tsx
@@ -1,5 +1,15 @@
-import { FC } from 'react';
-import { Breadcrumb, BreadcrumbItem } from '@patternfly/react-core';
+import { FC, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Dropdown,
+  DropdownItem,
+  DropdownPosition,
+  KebabToggle,
+  Level,
+  LevelItem,
+} from '@patternfly/react-core';
 import { useTranslation } from '../../i18n';
 
 export type BrokerDetailsBreadcrumbProps = {
@@ -12,21 +22,69 @@ const BrokerDetailsBreadcrumb: FC<BrokerDetailsBreadcrumbProps> = ({
   namespace,
 }) => {
   const { t } = useTranslation();
+  const [isOpen, setIsOpen] = useState(false);
+  const history = useHistory();
 
   let redirectPath: string;
   redirectPath = `/k8s/ns/${namespace}/brokers`;
 
   if (namespace === undefined) {
-    redirectPath = '/k8s/all-namespaces/brokers';
+    namespace = 'all-namespaces';
+    redirectPath = `/k8s/${namespace}/brokers`;
   }
 
+  const onClickEditBroker = () => {
+    history.push(`/k8s/ns/${namespace}/edit-broker/${name}`);
+  };
+
+  const onClickDeleteBroker = () => {
+    //Todo: add logic to open delete modal
+  };
+
+  const dropdownItems = [
+    <DropdownItem key="edit-broker" onClick={onClickEditBroker}>
+      {t('edit_broker')}
+    </DropdownItem>,
+    <DropdownItem key="delete-broker" onClick={onClickDeleteBroker}>
+      {t('delete_broker')}
+    </DropdownItem>,
+  ];
+
+  const onToggle = (isOpen: boolean) => {
+    setIsOpen(isOpen);
+  };
+
+  const onFocus = () => {
+    const element = document.getElementById('toggle-kebab');
+    element.focus();
+  };
+
+  const onSelect = () => {
+    setIsOpen(false);
+    onFocus();
+  };
+
   return (
-    <Breadcrumb className="pf-u-mb-md">
-      <BreadcrumbItem to={redirectPath}>{t('brokers')}</BreadcrumbItem>
-      <BreadcrumbItem isActive>
-        {t('broker')} {name}
-      </BreadcrumbItem>
-    </Breadcrumb>
+    <Level>
+      <LevelItem>
+        <Breadcrumb className="pf-u-mb-md">
+          <BreadcrumbItem to={redirectPath}>{t('brokers')}</BreadcrumbItem>
+          <BreadcrumbItem isActive>
+            {t('broker')} {name}
+          </BreadcrumbItem>
+        </Breadcrumb>
+      </LevelItem>
+      <LevelItem>
+        <Dropdown
+          onSelect={onSelect}
+          toggle={<KebabToggle id="broker-toggle-kebab" onToggle={onToggle} />}
+          isOpen={isOpen}
+          isPlain
+          dropdownItems={dropdownItems}
+          position={DropdownPosition.right}
+        />
+      </LevelItem>
+    </Level>
   );
 };
 

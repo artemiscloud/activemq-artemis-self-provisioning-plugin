b194d1fa789ebfa849db77cca25b0ac00563f0f1
ajaypratap003 <ajay.pratap233@gmail.com>
2023-09-01 17:31:10 +0530

    update dockerfile

--- http-server.sh
+++ http-server.sh
@@ -6,4 +6,4 @@ PUBLIC_PATH="$1"
 shift
 SERVER_OPTS="$@"
 
-./node_modules/.bin/http-server $PUBLIC_PATH -p 9001 -c-1 --cors $SERVER_OPTS
+./node_modules/.bin/http-server $PUBLIC_PATH -p 8001 -c-1 --cors $SERVER_OPTS

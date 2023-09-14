3b21d261f617b572126fb71607783824e7706ca4
Msarawan <msarawan@redhat.com>
2023-09-14 17:58:44 +0530

    Added unit test for ChartPopover component

---
+++ src/metrics/components/ChartPopover/ChartPopover.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen, waitForI18n } from '../../../test-utils';
+import { ChartPopover } from '../ChartPopover/ChartPopover';
+
+describe('ChartPopover', () => {
+  it('should renders ChartPopover component correctly', async () => {
+    const comp = render(
+      <ChartPopover title="Memory Usage" description="memory usage" />,
+    );
+    await waitForI18n(comp);
+    expect(
+      screen.getByLabelText('chart_popover_icon_screenreader_text'),
+    ).toBeInTheDocument();
+  });
+});

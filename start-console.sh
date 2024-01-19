c43d01602a797fb427dbcff5d6f2c35d1498ad82
Marjina Sarawan <99248895+Msarawan@users.noreply.github.com>
2024-01-19 21:24:21 +0530

    Fix plugin loading issue in the openshift-console (#98)
    * Updted dynamic-plugin-sdk version and fixed issues

--- start-console.sh
+++ start-console.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-CONSOLE_IMAGE=${CONSOLE_IMAGE:="quay.io/openshift/origin-console:latest"}
+CONSOLE_IMAGE=${CONSOLE_IMAGE:="quay.io/openshift/origin-console:4.14.0"}
 CONSOLE_PORT=${CONSOLE_PORT:=9000}
 
 echo "Starting local OpenShift console..."

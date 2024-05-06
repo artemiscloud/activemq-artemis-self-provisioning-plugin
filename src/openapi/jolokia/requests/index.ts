f18c6015ebbff427a7767a9eea280284a1fab478
Thomas Lavocat <tlavocat@redhat.com>
2024-05-06 14:10:38 +0200

    [#128] enable codegen for react-query requests
    Use the https://github.com/7nohe/openapi-react-query-codegen project to
    generate the endpoints to query the api-server from its swagger
    deifnition.
    
    Use the command `yarn codegen` to generate the files.
    
    /!\  we need to use the version 0.4.1 of the generator to target
    react-query version 4. This is because as of for now the console is
    using React 17.

---
+++ src/openapi/jolokia/requests/index.ts
@@ -0,0 +1,15 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { BrokersResponse } from './models/BrokersResponse';
+export type { OperationRef } from './models/OperationRef';
+export type { SimpleResponse } from './models/SimpleResponse';
+
+export { DevelopmentService } from './services/DevelopmentService';
+export { JolokiaService } from './services/JolokiaService';
+export { SecurityService } from './services/SecurityService';
---
+++ src/openapi/jolokia/requests/models/OperationRef.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type OperationRef = {
+  /**
+   * The method signature
+   */
+  signature: string;
+  args?: Array<string>;
+};

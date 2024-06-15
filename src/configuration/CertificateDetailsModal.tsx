30f2bfe70780ebab3d35d399c3d8f251c632c158
Howard Gao <howard.gao@gmail.com>
2024-06-15 21:42:02 +0800

    [#171] Cert details page cannot parse certs with ECDSA algorithm
    Use @peculiar/x509 to parse certificates

--- src/configuration/CertificateDetailsModal.tsx
+++ src/configuration/CertificateDetailsModal.tsx
@@ -1,6 +1,4 @@
-import { FC } from 'react';
 import {
-  CodeBlock,
   CodeBlockCode,
   DescriptionList,
   DescriptionListDescription,
@@ -10,12 +8,255 @@ import {
   Stack,
   StackItem,
 } from '@patternfly/react-core';
-import { pki, util } from 'node-forge';
 import { useTranslation } from '../i18n';
+import x509, { TextConverter, TextObject } from '@peculiar/x509';
+import { FC, useEffect, useState } from 'react';
+import { AsnConvert } from '@peculiar/asn1-schema';
+import {
+  Certificate,
+  Version,
+  TBSCertificate,
+  AlgorithmIdentifier,
+} from '@peculiar/asn1-x509';
+
+const getCertVersion = (tbsCert: TBSCertificate) => {
+  return `${Version[tbsCert.version]} (${tbsCert.version})`;
+};
+
+const getPublicInfo = (publicKey: x509.PublicKey) => {
+  const textObj = publicKey.toTextObject();
+  return TextConverter.serialize(textObj);
+};
+
+const getSignatureValue = (asnCert: Certificate) => {
+  return asnCert.signatureValue;
+};
+
+interface ExtensionsListProps {
+  crt: x509.X509Certificate;
+  order: number;
+}
+
+//https://datatracker.ietf.org/doc/html/rfc5280
+interface ExtensionProps {
+  extension: x509.Extension;
+  index: number;
+}
+
+const Extension: FC<ExtensionProps> = ({ extension, index }) => {
+  const obj = extension.toTextObject();
+  let extTitle = extension.type;
+  if (obj[TextObject.NAME]) {
+    extTitle = obj[TextObject.NAME];
+  }
+
+  const getExtensionContent = () => {
+    const textObj = extension.toTextObject();
+    return TextConverter.serialize(textObj);
+  };
+
+  return (
+    <DescriptionListGroup>
+      <DescriptionListTerm>
+        <b>
+          {index + 1} - {extTitle}
+        </b>
+      </DescriptionListTerm>
+      <DescriptionListDescription>
+        <CodeBlockCode id="hash-code-content">
+          {getExtensionContent()}
+        </CodeBlockCode>
+      </DescriptionListDescription>
+    </DescriptionListGroup>
+  );
+};
+
+const ExtensionsList: FC<ExtensionsListProps> = ({ crt, order }) => {
+  return (
+    <>
+      {crt.extensions.map((ext, index) => (
+        <Extension extension={ext} index={index} key={'extenstions' + order} />
+      ))}
+    </>
+  );
+};
+
+export const formatArrayBuffer = (sig: ArrayBuffer) => {
+  const width = 16;
+
+  let wcnt = 0;
+  let result = '';
+  const uint8Array = new Uint8Array(sig);
+  for (let i = 0; i < uint8Array.length; i++) {
+    const hexv = uint8Array[i].toString(16);
+    result += hexv.padStart(2, '0') + ':';
+    wcnt++;
+    if (wcnt === width) {
+      wcnt = 0;
+      result += '\n';
+    }
+  }
+
+  return result;
+};
+
+const formatSignatureAlgorighm = (signature: AlgorithmIdentifier) => {
+  const txtObject = TextConverter.serializeAlgorithm(signature);
+  return TextConverter.serialize(txtObject);
+};
+
+interface CertInfoProps {
+  cert: x509.X509Certificate;
+  order: number;
+}
+const CertInfo: FC<CertInfoProps> = ({ cert, order }) => {
+  const asnCert = AsnConvert.parse(cert.rawData, Certificate);
+  const tbsCert = asnCert.tbsCertificate;
+
+  const [selfSigned, setSelfSigned] = useState('false');
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    cert
+      .isSelfSigned()
+      .then((value) => {
+        setSelfSigned(value ? 'true' : 'false');
+      })
+      .catch(() => {
+        setSelfSigned('unknown');
+      });
+  }, [cert]);
+
+  return (
+    <>
+      <DescriptionList isHorizontal key={'dl-cert-info' + order}>
+        <DescriptionListGroup key={'version' + order}>
+          <DescriptionListTerm>
+            <b>{t('version')}</b>
+          </DescriptionListTerm>
+          <DescriptionListDescription>
+            {getCertVersion(tbsCert)}
+          </DescriptionListDescription>
+        </DescriptionListGroup>
+
+        <DescriptionListGroup key={'selfSigned' + order}>
+          <DescriptionListTerm>
+            <b>{t('self_signed')}</b>
+          </DescriptionListTerm>
+          <DescriptionListDescription>{selfSigned}</DescriptionListDescription>
+        </DescriptionListGroup>
+
+        <DescriptionListGroup key={'serialNumber' + order}>
+          <DescriptionListTerm>
+            <b>{t('serial_number')}</b>
+          </DescriptionListTerm>
+          <DescriptionListDescription>
+            <code>{tbsCert.serialNumber}</code>
+          </DescriptionListDescription>
+        </DescriptionListGroup>
+
+        <DescriptionListGroup key={'sigAlgorithm' + order}>
+          <DescriptionListTerm>
+            <b>{t('sig_algorithm')}</b>
+          </DescriptionListTerm>
+          <DescriptionListDescription>
+            <code>{formatSignatureAlgorighm(tbsCert.signature)}</code>
+          </DescriptionListDescription>
+        </DescriptionListGroup>
+
+        <DescriptionListGroup key={'siginfo' + order}>
+          <DescriptionListTerm>
+            <b>{t('sig_hash')}</b>
+          </DescriptionListTerm>
+          <DescriptionListDescription>
+            <CodeBlockCode id="hash-code-content">
+              {formatArrayBuffer(getSignatureValue(asnCert))}
+            </CodeBlockCode>
+          </DescriptionListDescription>
+        </DescriptionListGroup>
+
+        <DescriptionListGroup key={'validity' + order}>
+          <DescriptionListTerm>
+            <b>{t('validity')}</b>
+          </DescriptionListTerm>
+          <DescriptionListDescription>
+            <b>{t('not_before')} </b>
+            <code>{tbsCert.validity.notBefore.getTime().toUTCString()}</code>
+            <p />
+            <b>{t('not_after')} </b>
+            <code>{tbsCert.validity.notAfter.getTime().toUTCString()}</code>
+          </DescriptionListDescription>
+        </DescriptionListGroup>
+
+        <DescriptionListGroup key={'issuer' + order}>
+          <DescriptionListTerm>
+            <b>{t('issuer')}</b>
+          </DescriptionListTerm>
+          <DescriptionListDescription>
+            <code>{cert.issuer}</code>
+          </DescriptionListDescription>
+        </DescriptionListGroup>
+
+        <DescriptionListGroup key={'subject' + order}>
+          <DescriptionListTerm>
+            <b>{t('subject')}</b>
+          </DescriptionListTerm>
+          <DescriptionListDescription>
+            <code>{cert.subject}</code>
+          </DescriptionListDescription>
+        </DescriptionListGroup>
+
+        <DescriptionListGroup key={'publickey' + order}>
+          <DescriptionListTerm>
+            <b>{t('publickey_info')}</b>
+          </DescriptionListTerm>
+          <DescriptionListDescription>
+            <CodeBlockCode id="keydata-code-content">
+              {getPublicInfo(cert.publicKey)}
+            </CodeBlockCode>
+          </DescriptionListDescription>
+        </DescriptionListGroup>
+
+        {tbsCert.issuerUniqueID && (
+          <DescriptionListGroup key={'issuerId' + order}>
+            <DescriptionListTerm>
+              <b>{t('issuer_unique_id')}</b>
+            </DescriptionListTerm>
+            <DescriptionListDescription>
+              <code>{tbsCert.issuerUniqueID}</code>
+            </DescriptionListDescription>
+          </DescriptionListGroup>
+        )}
+        {tbsCert.subjectUniqueID && (
+          <DescriptionListGroup key={'subjectId' + order}>
+            <DescriptionListTerm>
+              <b>{t('subject_unique_id')}</b>
+            </DescriptionListTerm>
+            <DescriptionListDescription>
+              <code>{tbsCert.subjectUniqueID}</code>
+            </DescriptionListDescription>
+          </DescriptionListGroup>
+        )}
+
+        <DescriptionListGroup key={'extensions' + order}>
+          <DescriptionListTerm>
+            <b>
+              {t('extensions')}({cert.extensions?.length})
+            </b>
+            :
+          </DescriptionListTerm>
+          <DescriptionListDescription>
+            <ExtensionsList crt={cert} order={order}></ExtensionsList>
+          </DescriptionListDescription>
+        </DescriptionListGroup>
+      </DescriptionList>
+    </>
+  );
+};
 
 interface CertificateDetailsModalProps {
   isModalOpen: boolean;
-  certs: pki.Certificate[];
+  certs: x509.X509Certificate[];
   secretName: string;
   pem: string;
   onCloseModal: () => void;
@@ -29,320 +270,14 @@ const CertificateDetailsModal: FC<CertificateDetailsModalProps> = ({
   onCloseModal,
 }) => {
   const { t } = useTranslation();
-  const formatCertIssuer = (crt: pki.Certificate) => {
-    let result = '';
-    crt.issuer.attributes.forEach((attribute, i, all) => {
-      if (attribute.shortName) {
-        result += attribute.shortName;
-        result += ' = ';
-        result += attribute.value;
-        if (i !== all.length - 1) {
-          result += ', ';
-        }
-      }
-    });
-    return result;
-  };
-
-  const formatCertSubject = (crt: pki.Certificate) => {
-    let result = '';
-    crt.subject.attributes.forEach((attribute, i, all) => {
-      if (attribute.shortName) {
-        result += attribute.shortName;
-        result += ' = ';
-        result += attribute.value;
-        if (i !== all.length - 1) {
-          result += ', ';
-        }
-      }
-    });
-    return result;
-  };
-
-  interface ExtensionKeyProps {
-    extension: any;
-    extensionKey: string;
-  }
-
-  const ExtensionKey: FC<ExtensionKeyProps> = ({ extension, extensionKey }) => {
-    //https://datatracker.ietf.org/doc/html/rfc5280
-    const getExtPropContent = (ext: any, key: string) => {
-      let result = '';
-      if (typeof ext[key] === 'object') {
-        result += JSON.stringify(ext[key]);
-      } else {
-        result += ext[key].toString();
-      }
-      return result;
-    };
-    if (extensionKey === 'name' || extensionKey === 'value') {
-      return <></>;
-    }
-    return (
-      <p style={{ textIndent: '25px' }}>
-        <b>{extensionKey}: </b>
-        <code>{getExtPropContent(extension, extensionKey)}</code>
-      </p>
-    );
-  };
-  interface ExtensionProps {
-    extension: any;
-    index: number;
-  }
-
-  const Extension: FC<ExtensionProps> = ({ extension: ext, index }) => {
-    return (
-      <>
-        <DescriptionList>
-          <DescriptionListGroup>
-            <DescriptionListTerm>
-              <b>
-                {index + 1} - {ext.name}
-              </b>
-            </DescriptionListTerm>
-          </DescriptionListGroup>
-        </DescriptionList>
-        {[
-          Object.keys(ext).map((key) => (
-            <ExtensionKey
-              extension={ext}
-              extensionKey={key}
-              key={'extension' + ext.name + key}
-            />
-          )),
-        ]}
-      </>
-    );
-  };
-  interface ExtensionsListProps {
-    crt: pki.Certificate;
-    order: number;
-  }
-
-  const ExtensionsList: FC<ExtensionsListProps> = ({ crt, order }) => {
-    return (
-      <>
-        {crt.extensions.map((ext, index) => (
-          <Extension
-            extension={ext}
-            index={index}
-            key={'extenstions' + order}
-          />
-        ))}
-      </>
-    );
-  };
-
-  const formatArray = (obj: Array<any>, indent = ''): string => {
-    let result = '';
-    const width = 6;
-    let wcnt = 0;
-    result += indent;
-    for (let i = 0; i < obj.length; i++) {
-      result += obj[i] + ',';
-      wcnt++;
-      if (wcnt === width) {
-        wcnt = 0;
-        result += '\n' + indent;
-      }
-    }
-    return result + '\n';
-  };
-
-  const formatObject = (obj: any, indent = ''): string => {
-    let result = '';
-    if (obj instanceof Array) {
-      const output = formatArray(obj, indent);
-      result += output;
-    } else if (obj instanceof Object) {
-      for (const [key, value] of Object.entries(obj)) {
-        if (value instanceof Function) {
-          continue;
-        }
-        result += indent + key + ':';
-        if (value instanceof Object) {
-          result += '\n' + formatObject(value, indent + '  ');
-        } else {
-          result += value + '\n';
-        }
-      }
-    } else {
-      result = indent + obj + '\n';
-    }
-    return result;
-  };
-
-  const formatPublicKey = (publicKey: pki.PublicKey) => {
-    return formatObject(publicKey);
-  };
-
-  const formatSignature = (sig: any) => {
-    const width = 16;
-    const uint8Data = util.binary.raw.decode(sig);
-
-    let wcnt = 0;
-    let result = '';
-    for (let i = 0; i < uint8Data.length; i++) {
-      let hexv = uint8Data[i].toString(16);
-      if (hexv.length === 1) {
-        hexv = '0' + hexv;
-      }
-      result += hexv + ':';
-      wcnt++;
-      if (wcnt === width) {
-        wcnt = 0;
-        result += '\n';
-      }
-    }
-
-    return result;
-  };
-
-  const formatCertInfo = (crt: pki.Certificate, order: number) => {
-    return (
-      <DescriptionList isHorizontal key={'dl-cert-info' + order}>
-        <DescriptionListGroup key={'version' + order}>
-          <DescriptionListTerm>
-            <b>{t('version')}</b>
-          </DescriptionListTerm>
-          <DescriptionListDescription>{crt.version}</DescriptionListDescription>
-        </DescriptionListGroup>
-
-        <DescriptionListGroup key={'serialNumber' + order}>
-          <DescriptionListTerm>
-            <b>{t('serial_number')}</b>
-          </DescriptionListTerm>
-          <DescriptionListDescription>
-            <code>{crt.serialNumber}</code>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-
-        <DescriptionListGroup key={'issuer' + order}>
-          <DescriptionListTerm>
-            <b>{t('issuer')}</b>
-          </DescriptionListTerm>
-          <DescriptionListDescription>
-            <code>{formatCertIssuer(crt)}</code>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-
-        <DescriptionListGroup key={'issuerHash' + order}>
-          <DescriptionListTerm>
-            <b>{t('issuer_hash')}</b>
-          </DescriptionListTerm>
-          <DescriptionListDescription>
-            <code>{crt.issuer.hash}</code>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-
-        <DescriptionListGroup key={'validity' + order}>
-          <DescriptionListTerm>
-            <b>{t('validity')}</b>
-          </DescriptionListTerm>
-          <DescriptionListDescription>
-            <b>{t('not_before')}</b>
-            <code>{crt.validity.notBefore.toString()}</code>
-            <p />
-            <b>{t('not_after')}</b>
-            <code>{crt.validity.notAfter.toString()}</code>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-
-        <DescriptionListGroup key={'subject' + order}>
-          <DescriptionListTerm>
-            <b>{t('subject')}</b>
-          </DescriptionListTerm>
-          <DescriptionListDescription>
-            <code>{formatCertSubject(crt)}</code>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-
-        <DescriptionListGroup key={'subjectHash' + order}>
-          <DescriptionListTerm>
-            <b>{t('subject_hash')}</b>
-          </DescriptionListTerm>
-          <DescriptionListDescription>
-            <code>{crt.subject.hash}</code>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-
-        <DescriptionListGroup key={'signatureOid' + order}>
-          <DescriptionListTerm>
-            <b>{t('signature_oid')}</b>
-          </DescriptionListTerm>
-          <DescriptionListDescription>
-            <code>{crt.signatureOid}</code>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-
-        <DescriptionListGroup key={'signatue' + order}>
-          <DescriptionListTerm>
-            <b>{t('signature')}</b>
-          </DescriptionListTerm>
-          <DescriptionListDescription>
-            <CodeBlock
-              key={'key-codeblock-signature'}
-              id={'id-codeblock-signature'}
-              width={50}
-            >
-              <CodeBlockCode id="signature-code-content">
-                {formatSignature(crt.signature)}
-              </CodeBlockCode>
-            </CodeBlock>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-
-        <DescriptionListGroup key={'siginfo' + order}>
-          <DescriptionListTerm>
-            <b>{t('signature_info')}</b>
-          </DescriptionListTerm>
-          <DescriptionListDescription>
-            <b>{t('algorithm_oid')}</b>
-            <code>{crt.siginfo.algorithmOid}</code>
-            <p />
-            <b>{t('parameters')}</b>
-            <code>{JSON.stringify(crt.siginfo.parameters)}</code>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-
-        <DescriptionListGroup key={'extensions' + order}>
-          <DescriptionListTerm>
-            <b>
-              {t('extensions')}({crt.extensions?.length})
-            </b>
-            :
-          </DescriptionListTerm>
-          <DescriptionListDescription>
-            <ExtensionsList crt={crt} order={order}></ExtensionsList>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-
-        <DescriptionListGroup key={'publickey' + order}>
-          <DescriptionListTerm>
-            <b>{t('publickey_info')}</b>
-          </DescriptionListTerm>
-          <DescriptionListDescription>
-            <CodeBlock
-              key={'key-codeblock-publickey'}
-              id={'id-codeblock-publickey'}
-              width={50}
-            >
-              <CodeBlockCode id="publickey-code-content">
-                {formatPublicKey(crt.publicKey)}
-              </CodeBlockCode>
-            </CodeBlock>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-      </DescriptionList>
-    );
-  };
-
   const CertDetails: FC = () => {
     return (
       <>
         {certs.length > 0 &&
           certs.map((cert, index) => (
-            <StackItem key={index}>{formatCertInfo(cert, index)}</StackItem>
+            <StackItem key={index}>
+              <CertInfo cert={cert} order={index} />
+            </StackItem>
           ))}
       </>
     );
@@ -358,8 +293,6 @@ const CertificateDetailsModal: FC<CertificateDetailsModalProps> = ({
     >
       <Stack hasGutter key={'stack-cert-details'}>
         <CertDetails />
-      </Stack>
-      <Stack hasGutter key={'stack-pem'}>
         <StackItem>
           <DescriptionList isHorizontal key={'dl-pem'}>
             <DescriptionListGroup key={'dl-group-pem-key'}>
@@ -367,13 +300,7 @@ const CertificateDetailsModal: FC<CertificateDetailsModalProps> = ({
                 <b>{t('pem')}</b>
               </DescriptionListTerm>
               <DescriptionListDescription>
-                <CodeBlock
-                  key={'key-codeblock-pem'}
-                  id={'id-codeblock-pem'}
-                  width={50}
-                >
-                  <CodeBlockCode id="pem-code-content">{pem}</CodeBlockCode>
-                </CodeBlock>
+                <CodeBlockCode id="pem-code-content">{pem}</CodeBlockCode>
               </DescriptionListDescription>
             </DescriptionListGroup>
           </DescriptionList>

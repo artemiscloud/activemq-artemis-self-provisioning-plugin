68d3a19acce6a1cd6062965893f7d83a95f0ed61
Thomas Lavocat <tlavocat@redhat.com>
2024-07-02 16:46:26 +0200

    [NOISSUE] remove unused code

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,9 +8,6 @@ export const CERT_VERSION = 'v1';
 export const SECRET_APIGROUP = 'core';
 export const SECRET_VERSION = 'v1';
 
-export const UNASSIGNED_APPLICATIONS_KEY = '#UNASSIGNED_APP#';
-export const LABEL_PART_OF = 'app.kubernetes.io/part-of';
-export const DEFAULT_BROKER_NAME = 'default';
 export const chartHeight = 400;
 export const chartPadding = {
   bottom: 110, // Adjusted to accomodate legend

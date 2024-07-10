047f6f5f44f73d60ada5ad389868941723f53dfd
Thomas Lavocat <tlavocat@redhat.com>
2024-07-10 13:56:28 +0200

    [#193] match the plugin-template:latest config
    From https://github.com/openshift/console-plugin-template at commit
    2b4dbe4b4313e258be1754815614cf7d47f45c11
    
    The latest patternfly4 compatible versions where recovered from:
    https://github.com/openshift/console/blob/649af93d7cb60e7cd9007c4a9a69ef74ec791680/frontend/package.json#L141
    
    The package.json got sorted with npx sort-package-json for easier
    comparisons.
    
    Hack:
    For some reason, there's something strange happening with victory-core.
    If we remove this old configuration from the package.json file:
    
    ```
      "resolutions": {
        "victory-core": "<36.9.0"
      },
      "overrides": {
        "victory-core": "<36.9.0"
      },
    ```
    
    Then there are crashes running the metrics test suite (at the patternfly
    level). If we put it back, the UI won't render the metrics anymore.
    
    What is more surprising is that putting this section back, running yarn,
    removing it and running yarn once again **fixes both issues**. That's
    the reason why I'm leaving that piece of documentation in the commit, in
    case we need it back at some point.
    
    What we most probably need to do is to upgrade ASAP to patternfly 5.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,29 @@
 /* eslint-env node */
 
-import { Configuration as WebpackConfiguration } from 'webpack';
 import * as path from 'path';
+import { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 import { ConsoleRemotePlugin } from '@openshift-console/dynamic-plugin-sdk-webpack';
+
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 
-type Configuration = WebpackConfiguration;
+const isProd = process.env.NODE_ENV === 'production';
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
 
 const config: Configuration = {
-  mode: 'development',
-  // No regular entry points. The remote container entry is handled by ConsoleRemotePlugin.
+  mode: isProd ? 'production' : 'development',
+  // No regular entry points needed. All plugin related scripts are generated via ConsoleRemotePlugin.
   entry: {},
   context: path.resolve(__dirname, 'src'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name]-bundle.js',
-    chunkFilename: '[name]-chunk.js',
+    filename: isProd ? '[name]-bundle-[hash].min.js' : '[name]-bundle.js',
+    chunkFilename: isProd
+      ? '[name]-chunk-[chunkhash].min.js'
+      : '[name]-chunk.js',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
@@ -23,8 +31,8 @@ const config: Configuration = {
   module: {
     rules: [
       {
-        test: /\.(jsx?|tsx?|ts?)$/,
-        exclude: /node_modules/,
+        test: /\.(jsx?|tsx?)$/,
+        exclude: /\/node_modules\//,
         use: [
           {
             loader: 'ts-loader',
@@ -35,23 +43,38 @@ const config: Configuration = {
         ],
       },
       {
-        test: /\.s?(css)$/,
+        test: /\.(css)$/,
         use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|woff2?|ttf|eot|otf)(\?.*$|$)/,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/[name].[ext]',
+          filename: isProd ? 'assets/[contenthash][ext]' : 'assets/[name][ext]',
         },
       },
       {
-        test: /\.m?js/,
+        test: /\.(m?js)$/,
         resolve: {
           fullySpecified: false,
         },
       },
     ],
+  },
+  devServer: {
+    static: './dist',
+    port: 9001,
+    // Allow Bridge running in a container to connect to the plugin dev server.
+    allowedHosts: 'all',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, Content-Type, Authorization',
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
   },
   plugins: [
     new ConsoleRemotePlugin(),
@@ -59,23 +82,11 @@ const config: Configuration = {
       patterns: [{ from: path.resolve(__dirname, 'locales'), to: 'locales' }],
     }),
   ],
-  devtool: 'source-map',
+  devtool: isProd ? false : 'source-map',
   optimization: {
-    chunkIds: 'named',
-    minimize: false,
+    chunkIds: isProd ? 'deterministic' : 'named',
+    minimize: isProd,
   },
 };
-
-if (process.env.NODE_ENV === 'production') {
-  config.mode = 'production';
-  if (config.output) {
-    config.output.filename = '[name]-bundle-[hash].min.js';
-    config.output.chunkFilename = '[name]-chunk-[chunkhash].min.js';
-  }
-  if (config.optimization) {
-    config.optimization.chunkIds = 'deterministic';
-    config.optimization.minimize = true;
-  }
-}
 
 export default config;

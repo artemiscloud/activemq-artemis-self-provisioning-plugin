12884738d277366c5895e6bfffdb979a660bd725
Msarawan <msarawan@redhat.com>
2024-10-16 15:01:55 +0530

    [#306] update unit tests for Addresses comp
    Updated unit tests for all the components in the Addresses folder

--- src/addresses/Addresses/Address.component.test.tsx
+++ src/addresses/Addresses/Address.component.test.tsx
@@ -1,5 +1,8 @@
 import { render, screen } from '@app/test-utils';
 import { Addresses } from './Address.component';
+import { JolokiaAuthentication } from '@app/jolokia/components/JolokiaAuthentication';
+import { useJolokiaServiceGetAddresses } from '@app/openapi/jolokia/queries';
+import { useJolokiaLogin } from '@app/jolokia/customHooks';
 
 jest.mock('@openshift-console/dynamic-plugin-sdk', () => ({
   VirtualizedTable: jest.fn(({ Row, data, columns }) => (
@@ -16,22 +19,114 @@ jest.mock('@openshift-console/dynamic-plugin-sdk', () => ({
   )),
 }));
 
+jest.mock('../../openapi/jolokia/queries', () => ({
+  useJolokiaServiceGetAddresses: jest.fn(),
+}));
+
+jest.mock('../../jolokia/customHooks', () => ({
+  useJolokiaLogin: jest.fn(),
+  useGetApiServerBaseUrl: jest.fn(),
+}));
+
 jest.mock('./AddressRow', () => ({
   AddressRow: jest.fn(() => <div>AddressRow Component</div>),
 }));
 
+const mockUseJolokiaServiceReadAddressAttributes =
+  useJolokiaServiceGetAddresses as jest.Mock;
+const mockUseJolokiaLogin = useJolokiaLogin as jest.Mock;
+
 describe('Addresses', () => {
+  const mockData = [
+    { request: { Name: 'activemq.notification' }, RoutingType: 'MULTICAST' },
+    { request: { Name: 'DLQ' }, RoutingType: 'ANYCAST' },
+    { request: { Name: 'ExpiryQueue' }, RoutingType: 'ANYCAST' },
+    { request: { Name: 'sys.mqtt.sessions' }, RoutingType: 'ANYCAST' },
+  ];
+
   beforeEach(() => {
     jest.clearAllMocks();
+    mockUseJolokiaServiceReadAddressAttributes.mockReturnValue({
+      data: mockData,
+      isLoading: true,
+      isSuccess: true,
+      error: null,
+    });
+    mockUseJolokiaLogin.mockReturnValue({
+      token: 'mock-token',
+      isError: false,
+      source: 'api',
+    });
   });
 
-  it('should render the Addresses component', () => {
-    render(<Addresses isBrokerPod={true} />);
+  it('should render the Addresses title when isBrokerPod is true', () => {
+    mockUseJolokiaServiceReadAddressAttributes.mockReturnValueOnce({
+      data: mockData,
+      isLoading: false,
+      isSuccess: true,
+      error: null,
+    });
+    render(
+      <JolokiaAuthentication brokerCR={{ spec: {} }} podOrdinal={0}>
+        <Addresses isBrokerPod={true} />
+      </JolokiaAuthentication>,
+    );
     expect(screen.getByText('Addresses')).toBeInTheDocument();
   });
 
-  it('should render AddressRow component within the VirtualizedTable', () => {
-    render(<Addresses isBrokerPod={true} />);
-    expect(screen.getByText('AddressRow Component')).toBeInTheDocument();
+  it('should show the "This is not a Broker Pod" message when isBrokerPod is false', () => {
+    render(
+      <JolokiaAuthentication brokerCR={{ spec: {} }} podOrdinal={0}>
+        <Addresses isBrokerPod={false} />
+      </JolokiaAuthentication>,
+    );
+    expect(screen.getByText('This is not a Broker Pod')).toBeInTheDocument();
+  });
+
+  it('should display the loading state when data is being fetched', () => {
+    mockUseJolokiaServiceReadAddressAttributes.mockReturnValueOnce({
+      data: mockData,
+      isLoading: true,
+      isSuccess: true,
+      error: null,
+    });
+    render(
+      <JolokiaAuthentication brokerCR={{ spec: {} }} podOrdinal={0}>
+        <Addresses isBrokerPod={true} />
+      </JolokiaAuthentication>,
+    );
+    expect(screen.getByText('Loading')).toBeInTheDocument();
+  });
+
+  it('should display an error message if there is a load error', () => {
+    mockUseJolokiaServiceReadAddressAttributes.mockReturnValueOnce({
+      data: null,
+      isLoading: true,
+      isSuccess: false,
+      error: true,
+    });
+    render(
+      <JolokiaAuthentication brokerCR={{ spec: {} }} podOrdinal={0}>
+        <Addresses isBrokerPod={true} />
+      </JolokiaAuthentication>,
+    );
+    expect(
+      screen.getByText('Error while fetching addresses'),
+    ).toBeInTheDocument();
+  });
+
+  it('should render the AddressRow component within the VirtualizedTable when data is available', () => {
+    mockUseJolokiaServiceReadAddressAttributes.mockReturnValueOnce({
+      data: mockData,
+      isLoading: false,
+      isSuccess: true,
+      error: null,
+    });
+    render(
+      <JolokiaAuthentication brokerCR={{ spec: {} }} podOrdinal={0}>
+        <Addresses isBrokerPod={true} />
+      </JolokiaAuthentication>,
+    );
+    expect(screen.getAllByText('AddressRow Component')[0]).toBeInTheDocument();
   });
 });

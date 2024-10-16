d32ac8b8f0664375ae7d4dd5fe5ec4860dd2a7bf
Msarawan <msarawan@redhat.com>
2024-10-16 15:01:55 +0530

    [#306] update unit tests for Addresses comp
    Updated unit tests for all the components in the Addresses folder

--- src/addresses/Addresses/Address.container.test.tsx
+++ src/addresses/Addresses/Address.container.test.tsx
@@ -1,9 +1,8 @@
-import { render, screen } from '@app/test-utils';
+import { render, screen, waitForI18n } from '@app/test-utils';
 import { AddressContainer } from './Address.container';
-import { useJolokiaServiceGetAddresses } from '@app/openapi/jolokia/queries';
 import { useParams } from 'react-router-dom-v5-compat';
-import { JolokiaAuthentication } from '@app/jolokia/components/JolokiaAuthentication';
 import { useGetBrokerCR } from '@app/k8s/customHooks';
+import { JolokiaAuthentication } from '@app/jolokia/components/JolokiaAuthentication';
 import { useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
 
 jest.mock('@openshift-console/dynamic-plugin-sdk', () => ({
@@ -14,12 +13,8 @@ jest.mock('react-router-dom-v5-compat', () => ({
   useParams: jest.fn(),
 }));
 
-jest.mock('@app/k8s/customHooks', () => ({
+jest.mock('../../k8s/customHooks', () => ({
   useGetBrokerCR: jest.fn(),
-}));
-
-jest.mock('@app/openapi/jolokia/queries', () => ({
-  useJolokiaServiceGetAddresses: jest.fn(),
 }));
 
 jest.mock('./Address.component', () => ({
@@ -29,15 +24,13 @@ jest.mock('./Address.component', () => ({
 const mockUseParams = useParams as jest.Mock;
 const mockUseGetBrokerCR = useGetBrokerCR as jest.Mock;
 const mockUseK8sWatchResource = useK8sWatchResource as jest.Mock;
-const mockUseJolokiaServiceGetAddresses =
-  useJolokiaServiceGetAddresses as jest.Mock;
 
 describe('AddressContainer', () => {
   beforeEach(() => {
     jest.clearAllMocks();
     mockUseParams.mockReturnValue({
       ns: 'test-namespace',
-      name: 'DLQ',
+      name: 'ex-aao-ss-0',
     });
     mockUseK8sWatchResource.mockReturnValue([[], true, null]);
     mockUseGetBrokerCR.mockReturnValue({
@@ -57,35 +50,13 @@ describe('AddressContainer', () => {
     });
   });
 
-  it('should renders the Addresses component with data when fetching is successful', () => {
-    const mockAddresses = [
-      { name: 'DLQ', broker: { name: 'test-1' } },
-      { name: 'Expiry Queue', broker: { name: 'test-1' } },
-    ];
-
-    mockUseJolokiaServiceGetAddresses.mockReturnValue({
-      data: mockAddresses,
-      isSuccess: true,
-    });
-
-    render(
+  it('should render the Addresses component with data when fetching is successful', async () => {
+    const comp = render(
       <JolokiaAuthentication brokerCR={{ spec: {} }} podOrdinal={0}>
         <AddressContainer />
       </JolokiaAuthentication>,
     );
+    await waitForI18n(comp);
     expect(screen.getByText('Addresses Component')).toBeInTheDocument();
-  });
-
-  it('should does not render Addresses component when fetching fails', () => {
-    mockUseJolokiaServiceGetAddresses.mockReturnValue({
-      data: undefined,
-      isSuccess: false,
-    });
-    render(
-      <JolokiaAuthentication brokerCR={{ spec: {} }} podOrdinal={null}>
-        <AddressContainer />
-      </JolokiaAuthentication>,
-    );
-    expect(screen.queryByText('Addresses Component')).not.toBeInTheDocument();
   });
 });

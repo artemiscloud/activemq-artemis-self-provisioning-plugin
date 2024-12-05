0cda448795080b33d310081933e90600f30002ce
Michal T <mtoth@redhat.com>
2024-12-05 16:19:31 +0100

    [NO-ISSUE] Deployment should add plugin and remove on undeploy
    This should make sure we will not accidentally overwrite user deployed plugins
    deployment of our plugin. Also, undeployment of plugin should only remove our
    plugin from list of plugins.

--- undeploy-plugin.sh
+++ undeploy-plugin.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
 oc kustomize deploy/base | oc delete -f -
+
+# remove only our plugin from list of deployed plugins
+plugins=$(oc get consoles.operator.openshift.io cluster -o json | jq -c '.spec.plugins | map(select(. != "activemq-artemis-self-provisioning-plugin"))' )
+patchPath="{ \"spec\": { \"plugins\": $plugins } }"
+oc patch consoles.operator.openshift.io cluster --type=merge --patch "${patchPath}"

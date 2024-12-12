22b2ffaff70d7c5e1de9702e9958f7db5a9f769a
Msarawan <msarawan@redhat.com>
2024-12-12 12:16:18 +0530

    [#326] Add a YAML tab on the Broker details view
    Added a YAML tab on the Broker Details view to make it easier
    for users to access the Broker's details.

--- src/brokers/broker-details/BrokerDetails.container.tsx
+++ src/brokers/broker-details/BrokerDetails.container.tsx
@@ -4,10 +4,8 @@ import {
   Tab,
   TabTitleText,
   Title,
-  PageSection,
-  PageSectionVariants,
-  Alert,
   Spinner,
+  Alert,
 } from '@patternfly/react-core';
 import { useTranslation } from '@app/i18n/i18n';
 import { ClientsContainer } from './components/Clients/Clients.container';
@@ -20,12 +18,8 @@ import {
   JolokiaTestPanel,
 } from './components/JolokiaDevComponents';
 import { OverviewContainer } from './components/Overview/Overview.container';
-import { PodsContainer } from '@app/brokers/broker-details/components/broker-pods/PodsList.container';
-import {
-  useLocation,
-  useNavigate,
-  useParams,
-} from 'react-router-dom-v5-compat';
+import { PodsContainer } from './components/broker-pods/PodsList.container';
+import { useParams } from 'react-router-dom-v5-compat';
 import { JolokiaAuthentication } from '@app/jolokia/components/JolokiaAuthentication';
 import { useGetBrokerCR } from '@app/k8s/customHooks';
 import { AuthContext } from '@app/jolokia/context';
@@ -33,7 +27,9 @@ import { BrokerCR } from '@app/k8s/types';
 import {
   GreenCheckCircleIcon,
   RedExclamationCircleIcon,
+  HorizontalNav,
 } from '@openshift-console/dynamic-plugin-sdk';
+import { YamlContainer } from './components/yaml/Yaml.container';
 
 type AuthenticatedPageContentPropType = {
   brokerCr: BrokerCR;
@@ -50,54 +46,57 @@ const AuthenticatedPageContent: FC<AuthenticatedPageContentPropType> = ({
   error: errorBrokerCr,
 }) => {
   const { t } = useTranslation();
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const activeTabKey = searchParams.get('tab') || 'overview';
-  const navigate = useNavigate();
-  const handleTabSelect = (_event: any, eventKey: string | number) => {
-    searchParams.set('tab', eventKey.toString());
-    navigate({ search: searchParams.toString() });
-  };
+
   const {
     isSuccess: isSuccessToken,
     isLoading: isLoadingToken,
     isError: isErrorToken,
   } = useContext(AuthContext);
-  return (
-    <PageSection
-      variant={PageSectionVariants.light}
-      padding={{ default: 'noPadding' }}
-      className="pf-c-page__main-tabs"
-    >
-      <div className="pf-u-mt-md pf-u-mb-md">
-        <BrokerDetailsBreadcrumb name={name} namespace={namespace} />
-        <Title headingLevel="h2" className="pf-u-ml-md">
-          {t('Broker')} {name}
-        </Title>
-      </div>
-      {errorBrokerCr && <Alert variant="danger" title={errorBrokerCr} />}
-      <Tabs activeKey={activeTabKey} onSelect={handleTabSelect}>
-        <Tab
-          eventKey={'overview'}
-          title={<TabTitleText>{t('Overview')}</TabTitleText>}
-        >
+
+  const pages = [
+    {
+      href: '',
+      name: t('Overview'),
+      component: () =>
+        !loadingBrokerCr ? (
           <OverviewContainer
             name={name}
             namespace={namespace}
             cr={brokerCr}
             loading={loadingBrokerCr}
           />
-        </Tab>
-        <Tab
-          eventKey={'clients'}
-          title={<TabTitleText>{t('Clients')}</TabTitleText>}
-        >
-          <ClientsContainer />
-        </Tab>
-        <Tab eventKey={'pods'} title={<TabTitleText>{t('Pods')}</TabTitleText>}>
-          <PodsContainer />
-        </Tab>
-        {process.env.NODE_ENV === 'development' && (
+        ) : (
+          <Spinner size="md" />
+        ),
+    },
+    {
+      href: '/clients',
+      name: t('Clients'),
+      component: ClientsContainer,
+    },
+    {
+      href: '/pods',
+      name: t('Pods'),
+      component: PodsContainer,
+    },
+    {
+      href: '/yaml',
+      name: t('YAML'),
+      component: () =>
+        !loadingBrokerCr ? (
+          <YamlContainer brokerCr={brokerCr} />
+        ) : (
+          <Spinner size="md" />
+        ),
+    },
+  ];
+
+  if (process.env.NODE_ENV === 'development') {
+    pages.push(
+      {
+        href: `/jolokiaTestPanel`,
+        name: t('check-jolokia'),
+        component: () => (
           <Tab
             eventKey={'jolokiaTestPanel'}
             title={
@@ -120,8 +119,12 @@ const AuthenticatedPageContent: FC<AuthenticatedPageContentPropType> = ({
             <JolokiaTestPanel />
             <br />
           </Tab>
-        )}
-        {process.env.NODE_ENV === 'development' && (
+        ),
+      },
+      {
+        href: `/jolokia-details`,
+        name: t('jolokia-details'),
+        component: () => (
           <Tab
             eventKey={'jolokia-details'}
             title={
@@ -168,9 +171,22 @@ const AuthenticatedPageContent: FC<AuthenticatedPageContentPropType> = ({
               </Tab>
             </Tabs>
           </Tab>
-        )}
-      </Tabs>
-    </PageSection>
+        ),
+      },
+    );
+  }
+
+  return (
+    <>
+      <div className="pf-u-mt-md pf-u-mb-md">
+        <BrokerDetailsBreadcrumb name={name} namespace={namespace} />
+        <Title headingLevel="h2" className="pf-u-ml-md">
+          {t('Broker')} {name}
+        </Title>
+      </div>
+      {errorBrokerCr && <Alert variant="danger" title={errorBrokerCr} />}
+      <HorizontalNav pages={pages} />
+    </>
   );
 };
 
@@ -178,7 +194,6 @@ export const BrokerDetailsPage: FC = () => {
   const { ns: namespace, name } = useParams<{ ns?: string; name?: string }>();
 
   const { brokerCr, isLoading, error } = useGetBrokerCR(name, namespace);
-
   return (
     <>
       <JolokiaAuthentication brokerCR={brokerCr} podOrdinal={0}>
